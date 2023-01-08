b113b3f9dbb8e02b0d96a0f54187f483e4f98b74
Sao Kanneh <sheku.kanneh12@gmail.com>
2023-01-09 07:10:25 +1100

    added a test for getAccounts

--- src/client/UpClient.test.ts
+++ src/client/UpClient.test.ts
@@ -1,19 +1,36 @@
-import axios from 'axios';
+import mockAxios from 'jest-mock-axios';
 import UpClient from './UpClient';
 
-const createSpy = jest.spyOn(axios, 'create');
 
 describe('Up Client', () => {
-  it('should create axios instance with correct options', () => {
-    const client = new UpClient({
-      personalAccessToken: 'xyz',
+    it('should create axios instance with correct options', () => {
+        // tslint:disable-next-line:no-unused-expression
+        new UpClient({
+            personalAccessToken: 'xyz',
+        });
+        expect(mockAxios.create).toHaveBeenCalledWith({
+            baseURL: 'https://api.up.com.au/api/v1',
+            headers: {
+                Authorization: `Bearer  xyz`,
+            },
+        });
     });
 
-    expect(createSpy).toHaveBeenCalledWith({
-      baseURL: 'https://api.up.com.au/api/v1',
-      headers: {
-        Authorization: `Bearer  xyz`,
-      },
+    describe('getAccounts', () => {
+        it('should make GET call to /accounts when invoked',async () => {
+            mockAxios.get.mockResolvedValue({
+                data: {
+                    data: []
+                }
+            })
+
+            const client = new UpClient({
+                personalAccessToken: 'xyz'
+            });
+
+            await client.getAccounts();
+
+            expect(mockAxios.get).toHaveBeenCalledWith('/accounts')
+        });
     });
-  });
 });
