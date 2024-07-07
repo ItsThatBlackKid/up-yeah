dd2398537c2d254e775521b2acaf28a16fab230b
Sao Sheku Kanneh <sheku.kanneh12@gmail.com>
2024-07-07 18:30:31 +1000

    Revert "refactor: cleanup getAccount (#25)"
    This reverts commit 6c63fdd36c8b3c3ca7ec2e210c356ce92f40274d.

--- src/__mocks__/accountData.ts
+++ src/__mocks__/accountData.ts
@@ -62,31 +62,25 @@ export const mockUpAccountsResponse: GetAccountsResponse = {
 
 export const mockGetAccountResponse = {
 	data: {
-		data: [
-			{
-				type: 'accounts',
-				id: 'mockId',
-				attributes: {
-					displayName: 'up-yeah',
-					accountType: 'TRANSACTIONAL',
-					ownershipType: 'INDIVIDUAL',
-					balance: {
-						currencyCode: 'AUD',
-						value: '4.20',
-						valueInBaseUnits: 420,
-					},
-					createdAt: '2021-09-23T01:12:00+10:00',
+		data: {
+			type: 'accounts',
+			id: 'mockId',
+			attributes: {
+				displayName: 'up-yeah',
+				accountType: 'TRANSACTIONAL',
+				ownershipType: 'INDIVIDUAL',
+				balance: {
+					currencyCode: 'AUD',
+					value: '4.20',
+					valueInBaseUnits: 420,
 				},
-				relationships: {
-					transactions: {
-						data: [],
-					},
+				createdAt: '2021-09-23T01:12:00+10:00',
+			},
+			relationships: {
+				transactions: {
+					data: [],
 				},
 			},
-		],
-		links: {
-			prev: 'http://some.up.au/api/v1/account/endpoint/1',
-			next: 'http://some.up.au/api/v1/account/endpoint/2',
 		},
 	},
 };
