bb22438b1f91c04a53c7f7c9ffd7cabfbafbc491
Sao <sheku.kanneh12@gmail.com>
2024-07-19 02:17:02 +1000

    refactor: copy readme.md for NPM

--- scripts/npm-build.sh
+++ scripts/npm-build.sh
@@ -7,5 +7,6 @@ echo "lib directory created. Copying publish package.json"
 node ./scripts/updateNpmPackageJson.js
 
 cp package.npm.json lib/package.json
+cp README.md lib/README.md
 
 echo "copy complete"
