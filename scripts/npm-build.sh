98342235b9e52d78a0af5abcad3b094cd254120b
Sao Sheku Kanneh <sheku.kanneh12@gmail.com>
2024-07-19 00:02:25 +1000

    Revert "1.0.1 (#54)" (#55)
    This reverts commit 9b039773dc2dbd5e305e92c516fb79e552a7f4ee.

--- scripts/npm-build.sh
+++ scripts/npm-build.sh
@@ -6,7 +6,6 @@ echo "lib directory created. Copying publish package.json"
 
 node ./scripts/updateNpmPackageJson.js
 
-cp package.npm.json lib/package.json # copy package json made for NPM
-cp README.md lib/readme.md # copy README
+cp package.npm.json lib/package.json
 
 echo "copy complete"
