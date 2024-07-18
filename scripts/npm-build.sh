551666998b6adb6bd83e9bc000ae0e0a36eaa7f7
Sao <sheku.kanneh12@gmail.com>
2024-07-18 23:56:29 +1000

    ci: copy readme.md when building for npm.

--- scripts/npm-build.sh
+++ scripts/npm-build.sh
@@ -6,6 +6,7 @@ echo "lib directory created. Copying publish package.json"
 
 node ./scripts/updateNpmPackageJson.js
 
-cp package.npm.json lib/package.json
+cp package.npm.json lib/package.json # copy package json made for NPM
+cp README.md lib/readme.md # copy README
 
 echo "copy complete"
